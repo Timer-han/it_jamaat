1795f39bbc6bcad9d0070ed044bb115682931dd1
Timer-han <tpetrunnikov@mail.ru>
2025-06-21 18:17:47 +0300

    FIX: database for deploying

--- app/database/database.py
+++ app/database/database.py
@@ -8,6 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
